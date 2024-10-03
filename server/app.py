299c5f33d0eb352420d350c7d3030594e6b08a19
MNdagu <ndagumark@gmail.com>
2024-10-03 19:29:01 +0300

    Final

--- server/app.py
+++ server/app.py
@@ -21,6 +21,44 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    
+    if earthquake is None:
+        return make_response({"message": f"Earthquake {id} not found."}, 404)
+
+    response_data = {
+        "id": earthquake.id,
+        "location": earthquake.location,
+        "magnitude": earthquake.magnitude,
+        "year": earthquake.year
+    }
+    return make_response(response_data, 200)
+
+
+# New view to get earthquakes by minimum magnitude
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    response_data = {
+        "count": len(quakes),
+        "quakes": [
+            {
+                "id": quake.id,
+                "location": quake.location,
+                "magnitude": quake.magnitude,
+                "year": quake.year
+            }
+            for quake in quakes
+        ]
+    }
+    
+    return make_response(response_data, 200)
+    
+    
+    
 
 
 if __name__ == '__main__':
